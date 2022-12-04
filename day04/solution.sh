4757f81919faff68844d2fbb862da432998b5842
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2022-12-04 22:09:58 +0100

    Refactoring - one solve function

--- day04/solution.sh
+++ day04/solution.sh
@@ -5,68 +5,35 @@
   exit 1
 }
 
-write_sequences_to_files()
-{
-  SEQUENCE_PAIR="$1"
-
-  INTERVAL_1="$(cut -d',' -f1 <<<"$SEQUENCE_PAIR")"
-  INTERVAL_2="$(cut -d',' -f2 <<<"$SEQUENCE_PAIR")"
-
-  seq "${INTERVAL_1%-*}" "${INTERVAL_1#*-}" | sort > sequence1
-  seq "${INTERVAL_2%-*}" "${INTERVAL_2#*-}" | sort > sequence2
-}
-
-part1()
+solve()
 {
   COUNT=0
 
   while read -r line; do
 
-    write_sequences_to_files "$line"
+    INTERVAL_1="$(cut -d',' -f1 <<<"$line")"
+    INTERVAL_2="$(cut -d',' -f2 <<<"$line")"
 
-    FIRST_NOT_IN_SECOND="$(comm -32 sequence1 sequence2)"
-    SECOND_NOT_IN_FIRST="$(comm -31 sequence1 sequence2)"
+    seq "${INTERVAL_1%-*}" "${INTERVAL_1#*-}" | sort > sequence1
+    seq "${INTERVAL_2%-*}" "${INTERVAL_2#*-}" | sort > sequence2
 
-    if [ -z "$FIRST_NOT_IN_SECOND" ] || [ -z "$SECOND_NOT_IN_FIRST" ]; then
-      COUNT=$((COUNT + 1))
+    if [ "$part" = "part1" ]; then
+      FIRST_NOT_IN_SECOND="$(comm -32 sequence1 sequence2)"
+      SECOND_NOT_IN_FIRST="$(comm -31 sequence1 sequence2)"
+
+      if [ -z "$FIRST_NOT_IN_SECOND" ] || [ -z "$SECOND_NOT_IN_FIRST" ]; then
+        COUNT=$((COUNT + 1))
+      fi
+    else # part 2
+      OVERLAP="$(comm -12 sequence1 sequence2)"
+
+      if [ -n "$OVERLAP" ]; then
+        COUNT=$((COUNT + 1))
+      fi
     fi
   done <input.txt
 
   echo "$COUNT"
 }
 
-part2()
-{
-  COUNT=0
-
-  while read -r line; do
-
-    write_sequences_to_files "$line"
-
-    OVERLAP="$(comm -12 sequence1 sequence2)"
-
-    if [ -n "$OVERLAP" ]; then
-      COUNT=$((COUNT + 1))
-    fi
-  done <input.txt
-
-  echo "$COUNT"
-}
-
-cleanup()
-{
-  rm -f \
-    sequence{1,2}
-}
-
-main()
-{
-  if [ "$part" = "part1" ]; then
-    part1
-  else
-    part2
-  fi
-  cleanup
-}
-
-main
+solve
